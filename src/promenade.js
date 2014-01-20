4339ad6e113a43d403b11782fb931b8a58ad31ed
ericzou <ericzou@eqlink.com>
2014-01-20 15:28:14 -0800

    added plural support for promenade

--- src/promenade.js
+++ src/promenade.js
@@ -4,10 +4,10 @@
   define(['promenade/view', 'promenade/view/collection', 'promenade/view/form',
           'promenade/model', 'promenade/controller', 'promenade/application',
           'promenade/region', 'promenade/object', 'promenade/collection',
-          'promenade/delegation', 'promenade/sync', 'promenade/queue'],
+          'promenade/delegation', 'promenade/sync', 'promenade/queue', 'promenade/inflector'],
          function(View, CollectionView, FormView, Model,  Controller,
                   Application, Region, PromenadeObject, Collection,
-                  DelegationApi, SyncApi, QueueApi) {
+                  DelegationApi, SyncApi, QueueApi, InflectorApi) {
     return {
       Model: Model,
       Collection: Collection,
@@ -20,6 +20,7 @@
       Delegation: DelegationApi,
       Sync: SyncApi,
       Queue: QueueApi,
+      Inflector: InflectorApi,
       'Object': PromenadeObject
     };
   });

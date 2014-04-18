045a786d53c451f042eee8b9186e80a58a8bf9ea
Chris Joel <chris@scriptolo.gy>
2014-04-18 15:30:52 -0700

    Use state callbacks in favor of event system

--- test/promenade/test/view-spec.js
+++ test/promenade/test/view-spec.js
@@ -9,6 +9,14 @@ define(['backbone', 'promenade', 'promenade/view'],
     beforeEach(function() {
       MyView = View.extend({
         template: 'list',
+        states: {
+          initial: {
+            transitionTo: ['foo']
+          },
+          foo: {
+            transitionTo: []
+          }
+        },
         layout: {
           foo: '.foo',
           bar: '.bar'
@@ -43,6 +51,18 @@ define(['backbone', 'promenade', 'promenade/view'],
 
         myView = new MyView({
           model: myModel
+        });
+      });
+
+      it('has a className on its el that reflects the current state', function() {
+        expect(myView.$el.hasClass('state-initial')).to.be(true);
+      });
+
+      describe('and the state changes', function() {
+        it('changes the className on the el', function(){ 
+          myView.transitionTo('foo');
+          expect(myView.$el.hasClass('state-foo')).to.be(true);
+          expect(myView.$el.hasClass('state-initial')).to.be(false);
         });
       });
 
